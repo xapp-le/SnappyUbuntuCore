492e6201edc073030cb46920bae8bf7ac1218d07
Woodrow Shen <woodrow.shen@gmail.com>
2016-06-08 17:21:14 +0800

    Fix the uboot issue for missing dtb file

--- builder/gadget.mk
+++ builder/gadget.mk
@@ -25,7 +25,7 @@ u-boot:
 
 preload:
 	cd $(TOOLS_DIR)/utils && ./$(BOOTLOADER_PACK) $(PRELOAD_DIR)/bootloader.bin $(PRELOAD_DIR)/bootloader.ini $(OEM_BOOT_DIR)/bootloader.bin
-	mkenvimage -s 0x8000 -o $(OEM_BOOT_DIR)/uboot.env -r $(OEM_BOOT_DIR)/uEnv.txt
+	mkenvimage -r -s 131072  -o $(OEM_BOOT_DIR)/uboot.env $(OEM_BOOT_DIR)/uEnv.txt
 
 snappy:
 	snapcraft snap gadget
