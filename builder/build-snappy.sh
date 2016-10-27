b2a0d42ffb1f427d7f3e255cdf134e6a04a7895b
woodrow.shen <woodrow.shen@gmail.com>
2016-10-27 07:06:34 +0000

    Remove the deprecated files

--- builder/build-snappy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo ./tools/ubuntu-device-flash core 16 \
-	--gadget roseapple-pi \
-	--developer-mode \
-	--channel edge \
-	--kernel roseapple-pi-kernel \
-	--os ubuntu-core \
-	-o roseapple-pi-all-snap.img
