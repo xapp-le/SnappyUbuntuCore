c1cb6351a3a73525dcbfa41a2617fc681c83f328
Woodrow Shen <woodrow.shen@canonical.com>
2017-02-13 18:22:03 +0800

    Workaround to fix the image creation error caused by ubuntu-image

--- builder/build-snappy.sh
+++ builder/build-snappy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+SNAPPY_IMAGE=uc16-roseapple-pi-`date +%Y%m%d`-0.img
 
-sudo /snap/bin/ubuntu-image --channel stable --image-size 4G --extra-snaps bluez --extra-snaps snapweb --extra-snaps modem-manager --extra-snaps network-manager -o uc16-roseapple-pi-`date +%Y%m%d`-0.img roseapple.model
+sudo /snap/bin/ubuntu-image --channel stable --image-size 4G --extra-snaps roseapple-pi_16.04-1.2_armhf.snap --extra-snaps bluez --extra-snaps snapweb --extra-snaps modem-manager --extra-snaps network-manager -o ${SNAPPY_IMAGE} roseapple.model --debug
 
+sudo dd conv=notrunc if=./gadget/bootloader.bin of=${SNAPPY_IMAGE} seek=2097664 oflag=seek_bytes
+sudo dd conv=notrunc if=./gadget/u-boot.bin of=${SNAPPY_IMAGE} seek=3145728 oflag=seek_bytes
+
+if [ -f uc16-roseapple-pi-`date +%Y%m%d`-0.img ]; then
+	sudo xz -0 uc16-roseapple-pi-`date +%Y%m%d`-0.img
+fi
