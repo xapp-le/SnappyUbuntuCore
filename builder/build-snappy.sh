3a3d8211d5ecba25f4eaf0c3fa0593c13c198306
Woodrow Shen <woodrow.shen@canonical.com>
2017-05-04 16:30:15 +0800

    Update prebulid image for edge (core rev.1841)

--- builder/build-snappy.sh
+++ builder/build-snappy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 SNAPPY_IMAGE=uc16-roseapple-pi-`date +%Y%m%d`-0.img
 
-sudo /snap/bin/ubuntu-image --channel stable --image-size 4G --extra-snaps bluez --extra-snaps snapweb --extra-snaps modem-manager --extra-snaps network-manager -o ${SNAPPY_IMAGE} roseapple.model --debug
-
+sudo ubuntu-image -d -c edge roseapple.model
 #sudo dd conv=notrunc if=./gadget/bootloader.bin of=${SNAPPY_IMAGE} seek=2097664 oflag=seek_bytes
 #sudo dd conv=notrunc if=./gadget/u-boot.bin of=${SNAPPY_IMAGE} seek=3145728 oflag=seek_bytes
 
-if [ -f uc16-roseapple-pi-`date +%Y%m%d`-0.img ]; then
+if [ -f roseapple-pi.img ]; then
+	sudo mv roseapple-pi.img ${SNAPPY_IMAGE}
 	sudo xz -0 uc16-roseapple-pi-`date +%Y%m%d`-0.img
 fi
--- prebuild_download.md
+++ prebuild_download.md
@@ -1,5 +1,8 @@
 # Roseapple-Pi image download
 
+## edge
+- [uc16-roseapple-pi-20170505-0.img.xz](https://mega.nz/#!pzxBjL6D), key:[`!UXRcEj0D-L8H1rWb8oaZddE_53GrDR7yDUZWinbhKZw`]
+
 ## stable
 - [uc16-roseapple-pi-20170329-0.img.xz](https://mega.nz/#!Brg0WCxC), key:[`!KQ91ATTR4FgWrAK2bORSza0gVzVsGAvnubv77Lwp_zw`]
 
